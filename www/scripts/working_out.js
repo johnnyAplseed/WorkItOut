9b0122827ecc1112e4553b23104bf0f7a925ad66
johnnyAplseed <johnbldwrth92@gmail.com>
2019-06-22 21:05:50 -0400

    Calisthenic workout completed with functionality on set button

--- www/scripts/working_out.js
+++ www/scripts/working_out.js
@@ -30,19 +30,105 @@ var WorkingOutManager = (function () {
 
     }
 
-    this.lSitArray = ["STRAIGHT LEG HOLD", "SINGLE STRAIGHT LEG", "BENT KNEE HOLD", "STRAIGHT LEG RAISES", "BENT KNEE RAISES"];
-    var counter = 0;
+    this.lSitArray = ["STRAIGHT LEG RAISE - HOLD", "SINGLE STRAIGHT LEG RAISE - HOLD", "BENT KNEE RAISE - HOLD", "STRAIGHT LEG RAISES - AMRAP", "BENT KNEE RAISES - AMRAP"];
+    this.pullupArray = ["FULL LENGTH DROP PULLUPS - AMRAP", "JUMP ASSIST PULLUPS - AMRAP", "ISOMETRIC HOLD JUMP ASSIST PULLUPS - AMRAP", "RESISTANCE BANDS PULLUPS - AMRAP", "AUSSIE PULLUPS - AMRAP"];
+    this.dipArray = ["FIRM CORE DIP - AMRAP", "JUMP ASSIST DIPS - AMRAP", "SLOW LOWER NEGATIVE DIPS - AMRAP", "RESISTANCE BAND DIPS - AMRAP", "BEHIND THE BACK DIPS - AMRAP"];
+    this.pushupArray = ["WIDE SET PUSHUPS - AMRAP", "TIGHT SET LOW PUSHUPS - AMRAP", "WIDE SET KNEE PUSHUPS - AMRAP", "TIGHT SET LOW PUSHUPS - AMRAP", "DECLINE KNEE PUSHUPS - TIGHT SET LOW - AMRAP"];
+    this.squatArray = ["PISTOL SQUATS - ALTERNATE AT FAILURE - AMRAP", "ASSISTED PISTOL SQUAT - AMRAP", "EXPLOSIVE JUMP SQUATS - AMRAP", "BODY WEIGHT SQUATS - AMRAP"];
+    this.cardioArray = ["TIRE FLIPS - AMRAP", "LONG JUMPS - AMRAP", "TIRE JUMPS - AMRAP", "JUMP ROPE - AMRAP"];
+    this.lSitCounter = 0;
+    this.pullupCounter = 0;
+    this.dipCounter = 0;
+    this.pushupCounter = 0;
+    this.squatCounter = 0;
+    this.cardioCounter = 0;
+    this.exerciseCount = document.getElementsByClassName("wokrout_exercise").length;
+
 
     this.exerciseChanges = (id) => {
         var exs = document.getElementById(id);
-        console.log(this.lSitArray[counter]);
-
-        exs.classList.add("active");
         let self = this;
-        setTimeout(() => {
-            exs.innerText = counter === lSitArray.length ? "Finished: L-SITS" : this.lSitArray[counter++];
-            exs.classList.remove("active")
-        }, 100) //after 100 milliseconds change the background to black.
+
+        // console.log(id);
+
+        switch (id) {
+            case "workout1":
+                exs.classList.add("active");
+                setTimeout(() => {
+                    exs.innerText = lSitCounter === lSitArray.length ? "Finished: L-SITS" : this.lSitArray[lSitCounter++];
+                    exs.classList.remove("active")
+                }, 100) //after 100 milliseconds change the background to black.
+                if (lSitCounter >= lSitArray.length) {
+                    exs.classList.add("complete")
+                }
+                break;
+
+            case "workout2":
+
+                exs.classList.add("active");
+                // let self = this;
+                setTimeout(() => {
+                    exs.innerText = pullupCounter === pullupArray.length ? "Finished: PULLUPS" : this.pullupArray[pullupCounter++];
+                    exs.classList.remove("active")
+                }, 100) //after 100 milliseconds change the background to black.
+                if (pullupCounter >= pullupArray.length) {
+                    exs.classList.add("complete")
+                }
+                break;
+
+            case "workout3":
+
+                exs.classList.add("active");
+                // let self = this;
+                setTimeout(() => {
+                    exs.innerText = dipCounter === dipArray.length ? "Finished: DIPS" : this.dipArray[dipCounter++];
+                    exs.classList.remove("active")
+                }, 100) //after 100 milliseconds change the background to black.
+                if (dipCounter >= dipArray.length) {
+                    exs.classList.add("complete")
+                }
+                break;
+            case "workout4":
+
+                exs.classList.add("active");
+                // let self = this;
+                setTimeout(() => {
+                    exs.innerText = pushupCounter === pushupArray.length ? "Finished: PUSHUPS" : this.pushupArray[pushupCounter++];
+                    exs.classList.remove("active")
+                }, 100) //after 100 milliseconds change the background to black.
+                if (pushupCounter >= pushupArray.length) {
+                    exs.classList.add("complete")
+                }
+                break;
+
+            case "workout5":
+
+                exs.classList.add("active");
+                // let self = this;
+                setTimeout(() => {
+                    exs.innerText = squatCounter === squatArray.length ? "Finished: SQUATS" : this.squatArray[squatCounter++];
+                    exs.classList.remove("active")
+                }, 100) //after 100 milliseconds change the background to black.
+                if (squatCounter >= squatArray.length) {
+                    exs.classList.add("complete")
+                }
+                break;
+
+            case "workout6":
+                exs.classList.add("active");
+                // let self = this;
+                setTimeout(() => {
+                    exs.innerText = cardioCounter === cardioArray.length ? "Finished: CARDIO" : this.cardioArray[cardioCounter++];
+                    exs.classList.remove("active")
+                    // exs.innerHTML.add("<label></label>")
+                }, 100) //after 100 milliseconds change the background to black.
+                if (cardioCounter >= cardioArray.length) {
+                    exs.classList.add("complete")
+                }
+                break;
+            default: console.log('Something went wrong:('); break;
+        }
+
 
     }
 
@@ -63,33 +149,6 @@ var WorkingOutManager = (function () {
 
 
 
-// <div class="workout_exercise">
-//                 <div class="set_name">L-Sit Straight Leg</div>
-//                 <div class="set_exercise" id="workout1_set1" onclick="WorkingOutManager.toggleColor('workout1_set1')">TF
-//                 </div>
-//             </div>
-//             <div class="workout_exercise">
-//                 <div class="set_name">L-Sit One Leg Straight</div>
-//                 <div class="set_exercise" id="workout2_set2" onclick="WorkingOutManager.toggleColor('workout2_set2')">TF
-//                 </div>
-//                 <div class="set_exercise" id="workout2_set1" onclick="WorkingOutManager.toggleColor('workout2_set1')">TF
-//                 </div>
-//             </div>
-//             <div class="workout_exercise">
-//                 <div class="set_name">L-Sit Bent Legs</div>
-//                 <div class="set_exercise" id="workout3_set1" onclick="WorkingOutManager.toggleColor('workout3_set1')">TF
-//                 </div>
-//             </div>
-//             <div class="workout_exercise">
-//                 <div class="set_name">Straight Raises</div>
-//                 <div class="set_exercise" id="workout4_set1" onclick="WorkingOutManager.toggleColor('workout4_set1')">TF
-//                 </div>
-//             </div>
-//             <div class="workout_exercise">
-//                 <div class="set_name">Bent Raises</div>
-//                 <div class="set_exercise" id="workout5_set1" onclick="WorkingOutManager.toggleColor('workout5_set1')">TF
-//                 </div>
-//             </div>
 
 
 
