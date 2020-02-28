e3ce0777ebe3ba31a526a9f0f002d51c15155118
JOHNNYAPLSEED <johnbldwrth92@gmail.com>
2020-02-28 15:12:08 -0500

    corrected spaces issue on workouts generation

--- www/scripts/display_workouts.js
+++ www/scripts/display_workouts.js
@@ -5,8 +5,8 @@ window.onload = function () {
 
 	this.availableWorkouts = document.getElementById("workouts_wrapper").innerHTML;
 	for (var i = 0; i < workouts.length; i++) {
-		console.log(workouts[i].name.toString());
-		document.getElementById("workouts_wrapper").innerHTML += "<div><button class='workout_button' id=" + workouts[i].name.toString().replace(" ", "_") + " onClick=" + "loadWorkout('" + workouts[i].name.toString().replace(" ", "_") + "')>" + (workouts[i].name).toString() + "</button></div>"
+		console.log(workouts[i].name.toString().replace(/ /g, '_'));
+		document.getElementById("workouts_wrapper").innerHTML += "<div><button class='workout_button' id=" + workouts[i].name.toString().replace(/ /g, '_') + " onClick=" + "loadWorkout('" + workouts[i].name.toString().replace(/ /g, '_') + "')>" + (workouts[i].name).toString() + "</button></div>"
 	}
 
 	document.getElementById("workouts_wrapper").innerHTML += "<div></div><button class='workout_button' onclick=" + "window.location.href='" + "../additions/add_workout_table.html'>Add Workout</button></div>"
